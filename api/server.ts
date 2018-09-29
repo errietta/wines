ea836bd535e8a044e5e887359640e09257fa2be8
Errietta Kostala <errietta@errietta.me>
2018-09-29 12:32:09 +0100

    start api

---
+++ api/server.ts
@@ -0,0 +1,8 @@
+import * as express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
